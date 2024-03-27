ba306a0b4b4b1bfec8c36025f194a7f25b6062f9
Mauriani Maciel <maurianim@gmail.com>
2024-03-26 22:11:20 -0300

    add components

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
-import { ThemeProvider } from 'styled-components/native';
+import { ActivityIndicator, StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native'; 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+      { fontsLoaded ? <Groups/> : <Loading /> }
     </ThemeProvider>
   );
 }
